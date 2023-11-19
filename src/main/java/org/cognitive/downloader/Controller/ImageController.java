de4d8c6a78422ae17b832f35f9927e605ecd6f25
faez22338510 <faez.ham@gmail.com>
2023-11-19 15:55:56 +0330

    bug testing

--- src/main/java/org/cognitive/downloader/Controller/ImageController.java
+++ src/main/java/org/cognitive/downloader/Controller/ImageController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,4 +67,12 @@ public class ImageController {
         // Handle other errors
         return ResponseEntity.notFound().build();
     }
+
+    @RequestMapping("/test")
+    public String test(
+            @RequestParam String image
+    ) {
+        return image;
+    }
+
 }
---
+++ src/main/java/org/cognitive/downloader/Service/twitterService.java
@@ -0,0 +1,9 @@
+package org.cognitive.downloader.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class twitterService {
+
+
+}
