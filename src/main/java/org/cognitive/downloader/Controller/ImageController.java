1a07672645f806a4a585044bbea6b1692ea521b7
faez22338510 <faez.ham@gmail.com>
2024-08-29 12:01:05 +0330

    change address

--- src/main/java/org/cognitive/downloader/Controller/ImageController.java
+++ src/main/java/org/cognitive/downloader/Controller/ImageController.java
@@ -92,7 +92,7 @@ public class ImageController {
                         .body(resource);
             } else {
 //                imageDownloadService.downloadImage("http://85.185.67.243:8080/images?image=" + image,IMAGE_DIRECTORY + imageName);
-                imageDownloadService.downloadImage("http://49.12.245.57:8082/images?image=" + image,IMAGE_DIRECTORY + imageName);
+                imageDownloadService.downloadImage("http://49.13.30.62:8081?image=" + image,IMAGE_DIRECTORY + imageName);
                 return ResponseEntity.ok()
                         .headers(headers)
                         .body(new UrlResource(imagePath.toUri()));
