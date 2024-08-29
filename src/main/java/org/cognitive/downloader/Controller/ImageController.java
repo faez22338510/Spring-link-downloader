15b3d4010de1567f56b959fc01017681075035d3
faez22338510 <faez.ham@gmail.com>
2024-08-29 11:27:32 +0330

    change test controller

--- src/main/java/org/cognitive/downloader/Controller/ImageController.java
+++ src/main/java/org/cognitive/downloader/Controller/ImageController.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -110,7 +107,7 @@ public class ImageController {
         return ResponseEntity.notFound().build();
     }
 
-    @RequestMapping("/test")
+    @PostMapping("/test")
     public String test(
             @RequestParam String image
     ) {
